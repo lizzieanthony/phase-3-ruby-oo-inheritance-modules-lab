e2560b70084a795427ace441ddf00d43d9d0c60c
Lizzie <lizziewrab@me.com>
2022-06-14 18:35:04 -0600

    commit tree 431222401b5228f22b3296a62aa5a6b6c6e05875

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,11 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable:: InstanceMethods
+  include Memorable::InstanceMethods
+
   @@artists = []
 
   def initialize
@@ -14,29 +19,18 @@ class Artist
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
-
+  
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
